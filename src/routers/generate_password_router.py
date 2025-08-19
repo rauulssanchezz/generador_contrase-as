72bf8d459559516a64bcf4e2331010254674fb4e
raul <rauulssanchezz@gmail.com>
2025-08-19 11:20:42 +0200

    FEAT: hash password

--- src/routers/generate_password_router.py
+++ src/routers/generate_password_router.py
@@ -1,6 +1,6 @@
 from fastapi.routing import APIRouter
-from fastapi import Body
-from src.services.generate_password_service import GeneratePasswordService
+from fastapi import Query
+from src.services.password_service import PasswordService
 from src.utils.enums.complexity_enum import Complexity
 
 router = APIRouter(
@@ -8,6 +8,14 @@ router = APIRouter(
     tags=['Generate Password']
 )
 
-@router.post('/generate')
-def generate_password(complexity: Complexity = Body()):
-    return GeneratePasswordService.generate_password(complexity=complexity)
+@router.get('/generate')
+def generate_password(complexity: Complexity = Query(...)):
+    return PasswordService.generate_password(complexity=complexity)
+
+@router.post('/hash')
+def hash_password(password: str):
+    return PasswordService.hash_password(password=password)
+
+@router.post('/check')
+def check_password(password:str, hashed_password):
+    return PasswordService.check_password(password=password, hashed_password=hashed_password)
--- src/services/password_service.py
+++ src/services/password_service.py
@@ -1,7 +1,8 @@
 from random import randint
 from src.utils.enums.complexity_enum import Complexity
+import bcrypt
 
-class GeneratePasswordService:
+class PasswordService:
     @staticmethod
     def generate_password(complexity: Complexity):
         plain_password = ''
@@ -16,9 +17,18 @@ class GeneratePasswordService:
         elif Complexity.hard == complexity:
             characters = 'ABCDEFGHIJKLMNÑOPQRSTVWXYZ1234567890!|·#ª$%&/()¬?¿-_Ç*^.'
 
+        characters += characters.lower()
+
         for _ in range(complexity.value_num):
                 random_int = randint(0, len(characters) - 1)
                 plain_password += characters[random_int]
 
         return plain_password
 
+    @staticmethod
+    def hash_password(password: str):
+        return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+    
+    @staticmethod
+    def check_password(hashed_password: str, password: str):
+        return bcrypt.checkpw(password.encode(), hashed_password.encode())
