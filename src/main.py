6bf2b6b84bd349c1cbaf4258534e7a083566b344
raul <rauulssanchezz@gmail.com>
2025-08-19 13:19:21 +0200

    cors

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.routers.generate_password_router import router as generate_router
 
 app = FastAPI(
     title='Password Generator'
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(generate_router)
