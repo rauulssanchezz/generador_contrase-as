10b4506e4f563d6c68b0ec0fe4fae31a1e881cee
rauulssanchezz <rauulssanchezz@gmail.com>
2025-08-27 17:58:58 +0200

    add try excepts

--- src/services/password_service.py
+++ src/services/password_service.py
@@ -5,30 +5,41 @@ import bcrypt
 class PasswordService:
     @staticmethod
     def generate_password(complexity: Complexity):
-        plain_password = ''
-        characters = ''
+        try:
+            plain_password = ''
+            characters = ''
 
-        if Complexity.easy == complexity:
-            characters = 'ABCDEFGHIJKLMNÑOPQRSTVWXYZ'
-        
-        elif Complexity.medium == complexity:
-            characters = 'ABCDEFGHIJKLMNÑOPQRSTVWXYZ1234567890'
+            if Complexity.easy == complexity:
+                characters = 'ABCDEFGHIJKLMNÑOPQRSTVWXYZ'
+            
+            elif Complexity.medium == complexity:
+                characters = 'ABCDEFGHIJKLMNÑOPQRSTVWXYZ1234567890'
 
-        elif Complexity.hard == complexity:
-            characters = 'ABCDEFGHIJKLMNÑOPQRSTVWXYZ1234567890!|·#ª$%&/()¬?¿-_Ç*^.'
+            elif Complexity.hard == complexity:
+                characters = 'ABCDEFGHIJKLMNÑOPQRSTVWXYZ1234567890!|·#ª$%&/()¬?¿-_Ç*^.'
 
-        characters += characters.lower()
+            characters += characters.lower()
 
-        for _ in range(complexity.value_num):
-                random_int = randint(0, len(characters) - 1)
-                plain_password += characters[random_int]
+            for _ in range(complexity.value_num):
+                    random_int = randint(0, len(characters) - 1)
+                    plain_password += characters[random_int]
 
-        return plain_password
+            return plain_password
+        except:
+            return 'Ha ocurrido un error intentalo mas tarde.'
 
     @staticmethod
     def hash_password(password: str):
-        return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+        try:
+            return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+        except:
+            return 'Ha ocurrido un error intentalo mas tarde.'
     
     @staticmethod
     def check_password(hashed_password: str, password: str):
-        return bcrypt.checkpw(password.encode(), hashed_password.encode())
+        try:
+            return bcrypt.checkpw(password.encode(), hashed_password.encode())
+        except ValueError:
+            False
+        except:
+            return 'Ha ocurrido un error intentalo mas tarde.'
